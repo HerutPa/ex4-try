fcc6e9a8f93a06a40b4b76eea8277b7eba3a4156
Herut Partner <herutpa@std.hac.ac.il>
2025-10-19 09:19:36 +0200

    Initial commit

---
+++ src/main/java/com/example/ex4try/Ex4TryApplication.java
@@ -0,0 +1,13 @@
+package com.example.ex4try;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex4TryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex4TryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ex4try/Ex4TryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ex4try;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex4TryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
